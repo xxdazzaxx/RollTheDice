d15accd9997b7ece7dd79f0253dfb08d1828a10d
xxdazzaxx <cheries_luv@yahoo.com>
2017-08-21 16:33:38 -0400

    message

--- RollTheDice/ViewController.swift
+++ RollTheDice/ViewController.swift
@@ -12,14 +12,22 @@ class ViewController: UIViewController {
     
 
     
+    @IBOutlet weak var rollTheDice: UILabel!
     @IBOutlet weak var rollLabel: UILabel!
-
+    @IBOutlet weak var shakeLabel: UILabel!
+   
     let numbers = ["0", "1", "2", "3", "4", "5", "6"]
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    let newLayer = CAGradientLayer()
+        newLayer.colors = [UIColor.green.cgColor, UIColor.black.cgColor]
+        newLayer.frame = view.frame
+        
+        view.layer.insertSublayer(newLayer, at: 0)
     }
     
     override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
